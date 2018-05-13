acfd4c30e37122a16ca1369e259785eb36d60330
Chad Smith <chadsmith4@my.unt.edu>
2018-05-13 02:39:10 -0500

    covnerted app.js to use es6 classes

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-// Book Constructor
-// Handles creates the actual book object
-function Book(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-}
-
-function UI() {}
-
-// add a book to the list
-UI.prototype.addBook = function(book) {
-    const list = document.getElementById('book-list');
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${book.title}</td>
-                    <td>${book.author}</td>
-                    <td>${book.isbn}</td>
-                    <td><a href="#" class="delete">X</a></td>`;
-
-    list.appendChild(row);
-}
-
-// clears the inputs
-UI.prototype.clearInputs = function() {
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
-    document.getElementById('isbn').value = '';
-}
-
-// delete book
-UI.prototype.deleteRow = function(target) {
-    if(target.className === 'delete') {
-        target.parentElement.parentElement.remove();
+// the book class
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
 }
 
-// displays an alert, error or success, in the UI
-UI.prototype.showAlert = function(msg, className) {
-    const div = document.createElement('div');
-    div.className = `alert ${className}`;
-    const text = document.createTextNode(msg);
-    div.appendChild(text);
+// class for the UI
+class UI {
+    // adds a book to the table.
+    addBook(book) {
+        const list = document.getElementById('book-list');
+        const row = document.createElement('tr');
+        row.innerHTML = 
+            `<td>${book.title}</td>
+             <td>${book.author}</td>
+             <td>${book.isbn}</td>
+             <td><a href="#" class="delete">X</a></td>`;
 
-    const container = document.querySelector('.container');
-    const form = document.getElementById('book-form');
+        list.appendChild(row)
+    }
+
+    showAlert(msg, className) {
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+        const text = document.createTextNode(msg);
+        div.appendChild(text);
     
-    container.insertBefore(div, form);
+        const container = document.querySelector('.container');
+        const form = document.getElementById('book-form');
+        
+        container.insertBefore(div, form);
+    
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 
-    setTimeout(function(){
-        document.querySelector('.alert').remove();
-    }, 3000);
+    clearInputs() {
+        document.getElementById('title').value = '';
+        document.getElementById('author').value = '';
+        document.getElementById('isbn').value = '';
+    }
+
+    deleteRow(target) {
+        if(target.className === 'delete') {
+            target.parentElement.parentElement.remove();
+        }
+    }
 }
 
 // Event Listeners
@@ -76,5 +76,4 @@ document.getElementById('book-list').addEventListener('click', function(e){
     const ui = new UI();
     ui.deleteRow(e.target);
     ui.showAlert("Book Removed!", 'success');
-    
 });
